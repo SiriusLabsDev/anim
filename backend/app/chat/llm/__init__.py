49c99dbd233b1c12fb193aeac37f30e715197533
Shrey <sshrey0007@gmail.com>
2025-11-15 12:02:18 +0530

    add: claude haiku for title generation

--- backend/app/chat/llm/__init__.py
+++ backend/app/chat/llm/__init__.py
@@ -6,26 +6,31 @@ from app.config import config
 
 model = (
     ChatGoogleGenerativeAI(
-        model="gemini-2.5-pro", 
-        max_tokens=8192 * 4, 
-        temperature=0.1, 
+        model="gemini-2.5-pro",
+        max_tokens=8192 * 4,
+        temperature=0.1,
         api_key=config["GOOGLE_API_KEY"],
-        thinking_budget=4096
-    ) 
-    if config['LLM'] == 'GEMINI' 
-    else 
-    ChatAnthropic(
-        model="claude-sonnet-4-5-20250929", 
-        max_tokens=8192 * 3, 
-        temperature=0.1, 
-        api_key=config["CLAUDE_API_KEY"]
+        thinking_budget=4096,
+    )
+    if config["LLM"] == "GEMINI"
+    else ChatAnthropic(
+        model="claude-sonnet-4-5-20250929",
+        max_tokens=8192 * 3,
+        temperature=0.1,
+        api_key=config["CLAUDE_API_KEY"],
     )
 )
 
-parser = MarkdownPythonParser('./output')
+parser = MarkdownPythonParser("./output")
 
-scripting_model = ChatGoogleGenerativeAI(
-    model="gemini-2.0-flash", 
-    max_tokens=8192, 
-    api_key=config["GOOGLE_API_KEY"]
+scripting_model = (
+    ChatGoogleGenerativeAI(
+        model="gemini-2.0-flash", max_tokens=8192, api_key=config["GOOGLE_API_KEY"]
+    )
+    if config["LLM"] == "GEMINI"
+    else ChatAnthropic(
+        model="claude-3-5-haiku-20241022",
+        max_tokens=8192,
+        api_key=config["CLAUDE_API_KEY"],
+    )
 )
